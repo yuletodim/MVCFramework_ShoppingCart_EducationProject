afae8ea022de762b9e007cc3f6fa5b56a139e7ed
yuletodim <yuletodim@yahoo.es>
2015-10-02 22:24:44 +0300

    Styles added

--- ShoppingCart/views/layouts/default.php
+++ ShoppingCart/views/layouts/default.php
@@ -3,9 +3,18 @@
 <head>
     <title><?= $this->title; ?></title>
     <meta charset="UTF-8">
+    <!-- Load the styles -->
+    <link rel= "stylesheet" href="../../ShoppingCart/public/css/bootstrap.css"/>
+    <link rel= "stylesheet" href="../../ShoppingCart/public/css/bootstrap-theme-superhero.css"/>
     <link rel= "stylesheet" type="text/css" href="../../ShoppingCart/public/css/styles.css"/>
 </head>
 <body>
+<div class="container" ng-if="anonymousUser">
+    <div class="row">
+        alabala
+    </div>
+
+</div>
     <?= $this->getLayoutData('body'); ?>
     <?= $this->getLayoutData('body2'); ?>
 </body>
